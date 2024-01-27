9a33290128f1bb40e1d68a8c678f0fa80c0acd5f
w2xi <43wangxi@gmail.com>
2024-01-27 16:02:26 +0800

    feat: :tada::tada::tada: support series feature options

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 const fs = require('fs')
+const { program } = require('commander')
+const package = require('../package.json')
 
 const map = {
   directory: '📁',
@@ -13,19 +15,49 @@ const characters = {
 	last: '└'
 }
 
-function toTree(path) {
-  const stats = fs.statSync(path)
+program
+  .name('treei')
+  .version(package.version)
+  .description('Generate a directory structure tree')
+  .option('-i, --ignore <ig>', 'ignore specific directory name')
+  .option('-d, --depth <depth>', 'specify the depth of output')
+  .option('-f, --only-folder', 'output folder only')
+  .option('--icon', 'output emoji icon, prefixing filename or directory')
+  .option('-o, --output <output>', 'export content into a file')
+  .parse(process.argv)
+
+const options = program.opts()
+
+let ignoreList = []
+if (options.ignore) {
+  ignoreList = options.ignore.replace(/\s*/g, '').split(/,|\|/)
+}
+
+// The difference between statSync and lstatSync
+// https://stackoverflow.com/questions/32478698/what-is-the-different-between-stat-fstat-and-lstat-functions-in-node-js
+
+function toTree(path, deep = 0) {
+  const stats = fs.lstatSync(path)
   
   if (stats.isDirectory()) {
-    const dir = fs.readdirSync(path)
-    const temp = dir.filter(child => child !== 'node_modules').map(child => {
-      return toTree(`${path}/${child}`)
-    })
+    let dir = fs.readdirSync(path)
+
+    if (ignoreList.length) {
+      dir = dir.filter(child => !ignoreList.includes(child))
+    }
+    if (options.onlyFolder) {
+      dir = dir.filter(child => {
+        const childStats = fs.lstatSync(`${path}/${child}`)
+        return childStats.isDirectory()
+      })
+    }
+    const children = dir.map(child => toTree(`${path}/${child}`, deep + 1))
     const dirName = path.split('/').pop()
+
     return {
       type: 'directory',
       name: dirName,
-      children: temp
+      children,
     }
   } else {
     return {
@@ -35,16 +67,27 @@ function toTree(path) {
   }
 }
 
-const result = toTree(process.cwd())
+const root = toTree(process.cwd())
+
+let depth
+if (options.depth && !Number.isNaN(parseInt(options.depth))) {
+  depth = parseInt(options.depth)
+}
+
+function isReachedDepth(deep) {
+  return typeof depth === 'number' && deep >= depth
+}
 
 let output = ''
 
 function generateTreeStructure(data, deep = 0) {
+  if (isReachedDepth(deep)) return
+
   data.forEach((item, index) => {
     const borderPrefix = (characters.border + ' '.repeat(3)).repeat(deep)
     let contentPrefix = index === data.length - 1 ? characters.last : characters.contain 
     contentPrefix += characters.line.repeat(2)
-    const content = `${map[item.type]} ${item.name}`
+    const content = options.icon ? `${map[item.type]}${item.name}` : `${item.name}`
     let currentLineStr = `${borderPrefix}${contentPrefix}${content}`
 
     currentLineStr += '\n'
@@ -60,7 +103,27 @@ function generateTreeStructure(data, deep = 0) {
   })
 }
 
-generateTreeStructure(result.children)
+generateTreeStructure(root.children)
 
-console.log(result.name)
+if (options.output) {
+  let outputString = output
+  if (fileExistSync(options.output)) { // 追加处理
+    outputString = '\n' + output
+  }
+  fs.appendFile(options.output, outputString, (err) => {
+    if (err) throw err
+  })
+}
+
+// 检测文件或文件夹是否存在
+function fileExistSync(path) {
+  try {
+    fs.accessSync(path, fs.constants.F_OK)
+  } catch(e) {
+    return false
+  }
+  return true
+}
+
+console.log(root.name)
 console.log(output)
